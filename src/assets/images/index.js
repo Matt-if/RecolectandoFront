ac35e4e40ff6be7316ba0b7fb0b972f0cc0e89fb
Matias F <matiasfavorito@gmail.com>
2026-03-30 18:11:23 -0300

    added src images to repo

---
+++ src/assets/images/index.js
@@ -0,0 +1,35 @@
+// Logos
+import logoCompleto from './logos/logo_completo.avif'
+import logoRecortado from './logos/logo_recortado.png'
+import compostaje from './proyectoEnAccion/compostaje.avif'
+import recoleccion2 from './proyectoEnAccion/recoleccion2.avif'
+import pesaje from './proyectoEnAccion/pesaje.avif'
+import teamworkRamiro from './proyectoEnAccion/teamwork_ramiro.avif'
+import manosLombrices from './proyectoEnAccion/manos_lombrices.avif'
+import bolsitasCompostables from './proyectoEnAccion/bolsitas_compostables.avif'
+import tacho_verde from './tachos/tacho_verde.png'
+import tacho_negro from './tachos/tacho_negro.png'
+import tacho_verdosoAmarillo from './tachos/tacho_verdosoAmarillo.png'
+
+export const images = {
+  logos: {
+    logoCompleto,
+    logoRecortado
+  },
+  inicio: {
+    recoleccion2,
+    teamworkRamiro,
+    manosLombrices
+  },
+  nosotros: {
+    compostaje    
+  },
+  separacion_residuos: {
+    tacho_verde,
+    tacho_negro,
+    tacho_verdosoAmarillo
+  },
+  conocerMas: {
+    bolsitasCompostables
+  }
+}
